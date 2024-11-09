5bd2e02b7e2aa3dd5055c1dc7c9c817d02abb0dc
moong00n <thecodingmoon@gmail.com>
2024-11-09 11:21:50 -0500

    Update Bit Manipulation

--- bit_manipulation_basics/main.go
+++ bit_manipulation_basics/main.go
@@ -1,13 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-
-	binaryTen := 0b1010
-	decimalTen := 10
-
-	fmt.Printf("decimal ten to binary: %0b\n", binaryTen)
-	fmt.Printf("decimal ten to binary: %0b\n", decimalTen)
 
 }
